93b455ba09a28cc9664623dbd1dd30a100803ffe
JokerForCode <kandaness@163.com>
2024-02-08 23:30:11 +0800

    init project & add token.go function implement

---
+++ lexer/lexter_test.go
@@ -0,0 +1,40 @@
+package lexer
+
+import (
+	"testing"
+	"go-interpreter/token"
+)
+
+func　TestNextToken(t *testing.T) {
+	input := `=+(){},;`
+	
+	tests := []struct {
+		exceptedType token.TokenType
+		exceptedLiteral string
+	}{
+		{token.ASSIGN, "="},
+		{token.PLUS, "+"},
+		{token.LPAREN, "("},
+		{token.RPAREN, ")"},
+		{token.LBRACE, "{"},
+		{token.RBRACE, "}"},
+		{token.COMMA, ","},
+		{token.SEMICOLON, ";"},
+		{token.EOF, ""},
+	}
+
+	l := New(input)
+	for i, tt := range tests {
+		tok := l.NextToken()
+
+		if tok.Type != tt.exceptedType{
+			t.Fatalf("tests[%d] - tokentype wrong. expected=%q, got=%q",
+				i, tt.expectedType, tok.Type)
+		}
+
+		if tok.Literal != tt.exceptedLiteral {
+			t.Fatalf("tests[%d] - literal wrong. expected=%q, got=%q",
+			i, tt.expectedLiteral, tok.Literal)
+		}
+	}
+}
